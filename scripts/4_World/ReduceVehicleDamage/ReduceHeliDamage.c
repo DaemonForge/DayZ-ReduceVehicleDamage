5ddbbccb3bb4b14402f7cc94da592f92deed1165
Kevin Hoddinott <kevhoddinott@gmail.com>
2020-06-07 22:35:43 -0400

    Update ReduceHeliDamage.c

--- scripts/4_World/ReduceVehicleDamage/ReduceHeliDamage.c
+++ scripts/4_World/ReduceVehicleDamage/ReduceHeliDamage.c
@@ -10,19 +10,19 @@ modded class ExpansionHelicopterScript
 		if ( IsMissionHost())
 		{
 			if(ReduceVehicleDamageSettings.Get().debugLogs){
-				Print("[ReduceVehicleDamage] Called ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse);
+				Print("[ReduceVehicleDamage] Called ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse);
 			}
 			float dmg = extra.Impulse * m_dmgContactCoef;
 			float rvd_orgdmg = dmg;
 			if ( dmg < rvd_mindmg){
 				if(ReduceVehicleDamageSettings.Get().debugLogs && dmg > 100){
-					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than Min Damage - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse);
+					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than Min Damage - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse);
 				}
 				return;
 			}
 			if (!EngineIsOn() && rvd_nodmgoff){
 				if(ReduceVehicleDamageSettings.Get().debugLogs){
-					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse);
+					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse);
 				}
 				return;
 			}
@@ -38,13 +38,13 @@ modded class ExpansionHelicopterScript
 			if (dmg < 1) 
 			{
 				if(ReduceVehicleDamageSettings.Get().debugLogs){
-					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse);
+					Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse);
 				}
 				return;
 			}
 			extra.Impulse = dmg / m_dmgContactCoef;
 			if(ReduceVehicleDamageSettings.Get().debugLogs){
-				Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse);
+				Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse);
 			}
 		}
 		
