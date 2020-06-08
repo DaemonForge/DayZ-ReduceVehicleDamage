0b07829fbbf6e05ff3bef14a5c70611e4f181830
Kevin Hoddinott <kevhoddinott@gmail.com>
2020-06-08 01:33:10 -0400

    Adding minum damage for logging
    Prevents logs from filling too much

--- scripts/4_World/ReduceVehicleDamage/ReduceHeliDamage.c
+++ scripts/4_World/ReduceVehicleDamage/ReduceHeliDamage.c
@@ -2,22 +2,26 @@ modded class ExpansionHelicopterScript
 {
 	override void EOnContact( IEntity other, Contact extra ) 
 	{
-		float rvd_dmgModifier = ReduceVehicleDamageSettings.Get().helidmgModifier;
-		float rvd_mindmg = ReduceVehicleDamageSettings.Get().helimindmg;
-		bool rvd_subtractmindmg = ReduceVehicleDamageSettings.Get().subtractmindmg;
-		bool rvd_nodmgoff = ReduceVehicleDamageSettings.Get().nodmgifoff;
-
-		if ( IsMissionHost())
+		if ( IsMissionHost() && extra.Impulse > 0)
 		{
-			if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Called ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+
+		        float rvd_dmgModifier = ReduceVehicleDamageSettings.Get().helidmgModifier;
+		        float rvd_mindmg = ReduceVehicleDamageSettings.Get().helimindmg;
+		        bool rvd_subtractmindmg = ReduceVehicleDamageSettings.Get().subtractmindmg;
+		        bool rvd_nodmgoff = ReduceVehicleDamageSettings.Get().nodmgifoff;
+		        bool rvd_debug = ReduceVehicleDamageSettings.Get().debugLogs;
+
 			float dmg = extra.Impulse * m_dmgContactCoef;
+			
+			if ( rvd_debug && dmg > 10 ){ Print("[ReduceVehicleDamage] Called ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+
 			float rvd_orgdmg = dmg;
 			if ( dmg < rvd_mindmg){
-				if(ReduceVehicleDamageSettings.Get().debugLogs && dmg > 100){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than Min Damage - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+				if ( rvd_debug  && dmg > 10 ){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than Min Damage - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
 				return;
 			}
 			if (!EngineIsOn() && rvd_nodmgoff){
-				if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+				if ( rvd_debug && dmg > 10 ){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
 				return;
 			}
 			if (rvd_subtractmindmg)
@@ -31,11 +35,11 @@ modded class ExpansionHelicopterScript
 			}
 			if (dmg < 1) 
 			{
-				if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+				if ( rvd_debug ){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
 				return;
 			}
 			extra.Impulse = dmg / m_dmgContactCoef;
-			if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
+			if ( rvd_debug && dmg > 10){ Print("[ReduceVehicleDamage] Finished ExpansionHelicopterScript EOnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + extra.Impulse); }
 		}
 		
 		super.EOnContact( other, extra);
