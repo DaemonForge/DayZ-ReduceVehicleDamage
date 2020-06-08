ebc1c028103db0409fbcaf6de4707baed29bfcce
Kevin Hoddinott <kevhoddinott@gmail.com>
2020-06-08 01:40:24 -0400

    Updating logs to have min damage
    To prevent over logging

--- scripts/4_World/ReduceVehicleDamage/ReduceVehicleDamage.c
+++ scripts/4_World/ReduceVehicleDamage/ReduceVehicleDamage.c
@@ -2,22 +2,24 @@ modded class CarScript
 {
 	override void OnContact( string zoneName, vector localPos, IEntity other, Contact data )
 	{
-		float rvd_dmgModifier = ReduceVehicleDamageSettings.Get().dmgModifier;
-		float rvd_mindmg = ReduceVehicleDamageSettings.Get().mindmg;
-		bool rvd_subtractmindmg = ReduceVehicleDamageSettings.Get().subtractmindmg;
-		bool rvd_nodmgoff = ReduceVehicleDamageSettings.Get().nodmgifoff;
+		        
+		if ( GetGame().IsServer() && zoneName != "" && m_dmgContactCoef > 0 && data.Impulse > 0 )
+		{	
+		        float rvd_dmgModifier = ReduceVehicleDamageSettings.Get().dmgModifier;
+		        float rvd_mindmg = ReduceVehicleDamageSettings.Get().mindmg;
+		        bool rvd_subtractmindmg = ReduceVehicleDamageSettings.Get().subtractmindmg;
+		        bool rvd_nodmgoff = ReduceVehicleDamageSettings.Get().nodmgifoff;
+		        bool rvd_debug = ReduceVehicleDamageSettings.Get().debugLogs;
 
-		if ( GetGame().IsServer() && zoneName != "" && m_dmgContactCoef > 0 )
-		{		
-			if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Called CarScript OnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
+			if ( rvd_debug && dmg > 10 ){ Print("[ReduceVehicleDamage] Called CarScript OnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
 			float dmg = data.Impulse * m_dmgContactCoef;
 			float rvd_orgdmg = dmg;
 			if ( dmg < rvd_mindmg){
-				if(ReduceVehicleDamageSettings.Get().debugLogs && dmg > 100){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Damage is less than min - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
+				if( rvd_debug && dmg > 10 ){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Damage is less than min - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
 				return;
 			}
 			if (!EngineIsOn() && rvd_nodmgoff){
-				if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
+				if ( rvd_debug && dmg > 10 ){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Engine is off - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
 				return;
 			}
 			if (rvd_subtractmindmg)
@@ -31,11 +33,11 @@ modded class CarScript
 			}
 			if (dmg < 1) 
 			{
-				if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
+				if ( rvd_debug ){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Damage Less than one - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
 				return;
 			}
 			data.Impulse = dmg / m_dmgContactCoef;
-			if(ReduceVehicleDamageSettings.Get().debugLogs){ Print("[ReduceVehicleDamage] Finished CarScript OnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
+			if ( rvd_debug && dmg > 10 ) { Print("[ReduceVehicleDamage] Finished CarScript OnContact - Vechile Name: " + GetDisplayName() + " - Position: " + GetPosition() + " - Impulse is: " + data.Impulse); }
 		}
 		super.OnContact(zoneName, localPos, other, data);
 	}
